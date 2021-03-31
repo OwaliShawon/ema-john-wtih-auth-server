b24cacb25a617c5fa035a294816cea77852fdf61
Owali Ullah Shawon <33037708+OwaliShawon@users.noreply.github.com>
2021-04-01 00:26:48 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCh5a78xjg9cg8Yy0LwkK_FWdV1w9_oirE",
+  authDomain: "ema-john-with-auth-ph.firebaseapp.com",
+  projectId: "ema-john-with-auth-ph",
+  storageBucket: "ema-john-with-auth-ph.appspot.com",
+  messagingSenderId: "984771762353",
+  appId: "1:984771762353:web:7401a8a0bdffcc8a92ea90"
+};
+export default firebaseConfig;
